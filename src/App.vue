28a9d42716353f3380a26721e9da0f1ab25caf25
xcfkfkz <xcfkfkz@hotmail.com>
2021-11-04 19:13:11 +0800

    onload

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="masonry">
-    <div class="item" :key="i.id" v-for="i in data">
+  <div class="masonry" ref="parent">
+    <div class="item non-vis" :key="i.id" v-for="i in data">
       <img :src="i.img">
       <span class="title">{{i.value}}</span>
     </div>
@@ -14,27 +14,76 @@ export default {
     return {
       data
     };
+  },
+  mounted() {
+    this.setPos(this.$refs.parent)
+  },
+  methods: {
+    async setPos(parentNode) {
+      const children = parentNode.children;
+      let heightLeft = 0, heightRight = 0;
+      const align = async (childNode, where) => {
+        // 获取到父盒子实际宽度
+        const imgInChild = childNode.querySelector('img');
+        await this.imgLoaded(imgInChild);
+        const childHeight = parseFloat(window.getComputedStyle(childNode).height);
+        if (where === 'left') {
+          heightLeft += childHeight;
+          childNode.classList.add('float-left')
+        } else {
+          heightRight += childHeight;
+          childNode.classList.add('float-right')
+        }
+        childNode.classList.remove('non-vis')
+      };
+      for (let child of children) {
+        heightLeft <= heightRight
+          ? await align(child, 'left')
+          : await align(child, 'right')
+      }
+    },
+    async imgLoaded(imgNode) {
+      if (imgNode.complete) {
+        return true;
+      } else {
+        return new Promise(resolve => {
+          imgNode.addEventListener('load', () => {
+            resolve(true)
+          })
+        })
+      }
+    }
   }
 };
 </script>
 
-<style scoped>
-  .masonry {
-    column-count: 3;
-    column-gap: 10px;
-    padding: 10px;
-  }
-  .item {
-    break-inside: avoid;
-    border: 1px solid #999;
-    margin-bottom: 10px;
-  }
-  img {
-    width: 100%;
-    vertical-align: middle;
-  }
-  .title {
-    display: block;
-    margin-left: 5px;
-  }
+<style>
+.masonry {
+  overflow: hidden;
+  padding: 10px;
+}
+.item {
+  width: 45%;
+  border: 1px solid #999;
+  margin-bottom: 10px;
+  overflow: hidden;
+}
+.float-left {
+  float: left;
+}
+.float-right {
+  float: right;
+}
+.non-vis {
+  visibility: hidden;
+}
+img {
+  width: 100%;
+  /*height: 250px;*/
+  vertical-align: middle;
+}
+.title {
+  display: block;
+  margin-left: 5px;
+}
 </style>
